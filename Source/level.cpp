cc20dd9d99de423067ca8ffba8361e7f15d98d9e
ShinyLettuce <je03@live.se>
2024-02-23 17:02:28 +0100

    tiles with the number one are filled with a white box

--- Source/level.cpp
+++ Source/level.cpp
@@ -18,15 +18,26 @@ void Level::render()
 			WHITE
 		);
 	}
-	for (int j = 0; j < gridSide + 1; j++)
+	for (int i = 0; i < gridSide + 1; i++)
 	{
 		DrawLine
 		(
 			(int)gridPosition.x,
-			(int)gridPosition.y + j * tilePixelSide,
+			(int)gridPosition.y + i * tilePixelSide,
 			(int)gridPosition.x + tilePixelSide * gridSide,
-			(int)gridPosition.y + j * tilePixelSide,
+			(int)gridPosition.y + i * tilePixelSide,
 			WHITE
 		);
+	}
+
+	for (int i = 0; i < gridSide; i++)
+	{
+		for (int j = 0; j < gridSide; j++)
+		{
+			if (grid[j + i * gridSide] == 1)
+			{
+				DrawRectangle((int)gridPosition.x + j * tilePixelSide, (int)gridPosition.y + i * tilePixelSide, tilePixelSide,tilePixelSide,WHITE);
+			}
+		}
 	}
 }
